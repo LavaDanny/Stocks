0534331225516a58a3e6307df69c5920efd8cbee
Danny <dannyahnguyen@gmail.com>
2021-10-11 19:06:07 -0400

    adds stocks to sql db and updates if new values added

--- stock_demo.py
+++ stock_demo.py
@@ -5,29 +5,69 @@ import yfinance as yf
 import matplotlib.pyplot as plt
 import sqlite3
 from sqlite3 import Error
+import pandas as pd
 
-def create_connection(path):
-    connection = None
-    try:
-        connection = sqlite3.connect(path)
-        print("Connection to SQLite DB successful")
-    except Error as e:
-        print(f"The error '{e}' occurred")
+def download(bt_inputs, proxy = None):
+    data = yf.download(tickers= bt_inputs['tickers'],
+                       start = bt_inputs['start_date'],   
+                       end = bt_inputs['end_date'],
+                       interval = '1d',
+                       prepost = True,
+                       threads = True,
+                       proxy = proxy)
+    return data
 
-    return connection
+# backtest inputs
+bt_inputs = {'tickers': ['BA', 'UNH', 'MCD', 'HD'],
+'start_date': '2019-01-01',
+'end_date': '2021-08-01'}
 
-# Get the data for the SPY ETF by specifying the stock ticker, start date, and end date
-data = yf.download('SPY','2015-01-01','2015-01-10')
+# create a sql connection
+con = sqlite3.connect('stock.db')
+c = con.cursor()
 
-print(data)
-print('\n')
+# create price table
+# query1 = """CREATE TABLE IF NOT EXISTS prices (
+# Date TEXT NOT NULL,
+# ticker TEXT NOT NULL,
+# price REAL,
+# PRIMARY KEY(Date, ticker)
+# )"""
+# c.execute(query1.replace('\n',' '))
 
-for row in data.iterrows():
-    print(row[0])
+# create volume table
+# query2 = """CREATE TABLE IF NOT EXISTS volume (
+# Date TEXT NOT NULL,
+# ticker TEXT NOT NULL,
+# volume REAL,
+# PRIMARY KEY(Date, ticker)
+# )"""
+# c.execute(query2.replace('\n',' '))
 
-# Plot the close prices
-data["Adj Close"].plot()
-plt.show()
+test = download(bt_inputs)
 
-# Attempt to make connection and add to SQLite db
-connection = create_connection("C:\\sqlite\SQLiteStudio\demo.db")
+adj_close = test['Adj Close']
+volume = test['Volume']
+
+# convert wide to long
+adj_close_long = pd.melt(adj_close.reset_index(), id_vars='Date', value_vars=bt_inputs['tickers'], var_name ="ticker", value_name="price")
+volume_long = pd.melt(volume.reset_index(), id_vars='Date', value_vars=bt_inputs['tickers'], var_name = "ticker", value_name = "volume")
+
+adj_close_long.to_sql('prices', con, if_exists='replace', index=False)
+volume_long.to_sql('volume', con, if_exists='replace', index=False)
+
+# inputs
+select_tickers = bt_inputs['tickers']
+start_date = bt_inputs['start_date']
+end_date = bt_inputs['end_date']
+# construct query
+query = """
+select * from prices
+where ticker in ('"""+ "','".join(select_tickers) + """')
+and Date >= '"""+ start_date + """'
+and Date < '""" + end_date + "'"
+c.execute(query.replace('\n',' '))
+result = pd.DataFrame(c.fetchall(), columns = ['Date', 'ticker', 'price'])
+# convert to datetime
+result['Date'] = pd.to_datetime(result['Date'])
+
