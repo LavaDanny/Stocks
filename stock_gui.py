579dd78c2ab37c7dccd911a76bf1807687538b3a
Danny <dannyahnguyen@gmail.com>
2021-10-17 20:29:00 -0400

    sorted table data

--- stock_gui.py
+++ stock_gui.py
@@ -4,12 +4,15 @@ import tkinter as tk
 
 # create table using sql data (sellDates for now)
 class Table:
-    def __init__(self, window):
-        for i in range(rows):
-            for j in range(columns):
+    def __init__(self, window, data):
+
+        data.sort(key = lambda t: (t[0], t[2]))
+
+        for i in range(len(data)):
+            for j in range(len(data[0])):
                 self.e = tk.Entry(window, width=20, fg='blue', font=('Arial',16,'bold'))
                 self.e.grid(row = i, column = j)
-                self.e.insert(tk.END, sellDate[i][j])
+                self.e.insert(tk.END, data[i][j])
 
 # make db connection
 con = sqlite3.connect('C:\\Users\\LavaDanny\\Desktop\\Coding\\Stocks\\stock.db')
@@ -31,8 +34,8 @@ sellDate = []
 for row in c.fetchall():
     sellDate.append(row)
 
-rows = len(sellDate)
-columns = len(sellDate[0])
+# rows = len(sellDate)
+# columns = len(sellDate[0])
 
 #calculate net gain/loss
 
@@ -48,10 +51,11 @@ print(sum)
 window = tk.Tk()
 
 window.title('Hello Python')
-window.geometry("1000x200+10+20")
+window.geometry("1000x300")
 
 frame_data = tk.Frame(window)
-frame_data = Table(window)
+data = buyDate + sellDate
+frame_data = Table(window, data)
 
 
 window.mainloop()
