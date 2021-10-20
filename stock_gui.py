e608be756e9c81c7953c78ba1dbd506d99581f32
Danny <dannyahnguyen@gmail.com>
2021-10-20 14:03:32 -0400

    modularize features

--- stock_gui.py
+++ stock_gui.py
@@ -18,7 +18,56 @@ def remove_ticker(db_con, ticker, con):
     con.commit()
     con.close()
 
-# create table using sql data (sellDates for now)
+    # update table
+     
+# create table
+def create_table(db_con, data, window):
+
+    # get min and max price data
+    db_con.execute("SELECT ticker, price, MIN(Date)\
+    FROM prices GROUP BY ticker")
+
+    buyDate = []
+    for row in db_con.fetchall():
+        buyDate.append(row)
+
+    db_con.execute("SELECT ticker, price, MAX(Date)\
+    FROM prices GROUP BY ticker")
+
+    sellDate = []
+    for row in db_con.fetchall():
+        sellDate.append(row)
+
+    frame_data = Frame(window)
+    frame_data.grid(row = 0, column = 0)
+    data = buyDate + sellDate
+    frame_data = Table(window, data)
+
+# create ticker drop down
+def create_ticker_dropdown(db_con, window):
+    tickers = StringVar(window)
+    tickers_arr = []
+
+    db_con.execute("SELECT ticker, price, MIN(Date)\
+    FROM prices GROUP BY ticker")
+
+    buyDate = []
+    for row in db_con.fetchall():
+        buyDate.append(row)
+
+    for x in buyDate:
+        tickers_arr.append(x[0])
+    print(tickers_arr)
+
+    tickers.set(tickers_arr[0])
+    menu_ticker = OptionMenu(window, tickers, *tickers_arr)
+    menu_ticker.grid(row = 10, column = 1, padx = 10, pady = 10)
+
+    remove_btn = Button(window, text = "remove selected ticker", command = lambda: remove_ticker(c, tickers.get(), con))
+    remove_btn.grid(row = 10, column = 2)
+    #remove_btn.bind('<Button-1', remove_ticker(c))
+
+# class to create table using sql data (sellDates for now)
 class Table:
     def __init__(self, window, data):
 
@@ -30,40 +79,9 @@ class Table:
                 self.e.grid(row = i, column = j)
                 self.e.insert(END, data[i][j])
 
-
-
-
-
-
-
 # make db connection
 con = sqlite3.connect('C:\\Users\\LavaDanny\\Desktop\\Coding\\Stocks\\stock.db')
 c = connect_to_db(con)
-
-# get min and max price data
-c.execute("SELECT ticker, price, MIN(Date)\
-FROM prices GROUP BY ticker")
-
-buyDate = []
-for row in c.fetchall():
-    buyDate.append(row)
-
-c.execute("SELECT ticker, price, MAX(Date)\
-FROM prices GROUP BY ticker")
-
-sellDate = []
-for row in c.fetchall():
-    sellDate.append(row)
-
-#calculate net gain/loss
-
-i = 0
-sum = 0
-for x in range(len(buyDate)):
-    sum = sellDate[i][1] - buyDate[i][1] + sum
-    i += 1
-
-print(sum) 
 
 # tkinter window
 window = Tk()
@@ -72,26 +90,11 @@ window.title('Hello Python')
 window.geometry("1000x300")
 
 # create stock table
-frame_data = Frame(window)
-frame_data.grid(row = 0, column = 0)
-data = buyDate + sellDate
-frame_data = Table(window, data)
+data = []
+create_table(c, data, window)
 
 # tickers drop down
-tickers = StringVar(window)
-tickers_arr = []
-
-for x in buyDate:
-    tickers_arr.append(x[0])
-print(tickers_arr)
-
-tickers.set(tickers_arr[0])
-menu_ticker = OptionMenu(window, tickers, *tickers_arr)
-menu_ticker.grid(row = 10, column = 1, padx = 10, pady = 10)
-
-remove_btn = Button(window, text = "remove selected ticker", command = lambda: remove_ticker(c, tickers.get(), con))
-remove_btn.grid(row = 10, column = 2)
-#remove_btn.bind('<Button-1', remove_ticker(c))
+create_ticker_dropdown(c, window)
 
 # start window
 window.mainloop()
